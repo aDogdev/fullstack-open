13027075f353c3e4d6e9429656241b001e5a7b70
aDog <adrianmg3008@gmail.com>
2024-01-31 14:00:46 -0500

    add exercise 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -4,6 +4,16 @@ function Header({ course }) {
 
 function Content({ part, exercises }) {
   return (
+    <div>
+      <Part part={part[0]} exercises={exercises[0]} />
+      <Part part={part[1]} exercises={exercises[1]} />
+      <Part part={part[2]} exercises={exercises[2]} />
+    </div>
+  );
+}
+
+function Part({ part, exercises }) {
+  return (
     <p>
       {part} {exercises}
     </p>
@@ -26,9 +36,10 @@ function App() {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content
+        part={[part1, part2, part3]}
+        exercises={[exercises1, exercises2, exercises3]}
+      />
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
