42696f6916cbb11c8a24b09e2db41fe0c4714b60
aDog <adrianmg3008@gmail.com>
2024-02-02 09:47:46 -0500

    add exercise 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,49 +2,53 @@ function Header({ course }) {
   return <h1>{course}</h1>;
 }
 
-function Content({ part, exercises }) {
+function Content({ part }) {
   return (
     <div>
-      <Part part={part[0]} exercises={exercises[0]} />
-      <Part part={part[1]} exercises={exercises[1]} />
-      <Part part={part[2]} exercises={exercises[2]} />
+      <Part part={part.part1} />
+      <Part part={part.part2} />
+      <Part part={part.part3} />
     </div>
   );
 }
 
-function Part({ part, exercises }) {
+function Part({ part }) {
   return (
     <p>
-      {part} {exercises}
+      {part.name}: {part.exercises}
     </p>
   );
 }
 
-function Total({ exercises1, exercises2, exercises3 }) {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+function Total({ part }) {
+  return (
+    <p>
+      Number of exercises{" "}
+      {part.part1.exercises + part.part2.exercises + part.part3.exercises}
+    </p>
+  );
 }
 
 function App() {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part={[part1, part2, part3]}
-        exercises={[exercises1, exercises2, exercises3]}
-      />
-      <Total
-        exercises1={exercises1}
-        exercises2={exercises2}
-        exercises3={exercises3}
-      />
+      <Content part={{ part1, part2, part3 }} />
+      <Total part={{ part1, part2, part3 }} />
     </div>
   );
 }
