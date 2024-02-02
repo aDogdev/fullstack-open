008e51745a97f48fffe156f61a7bbb18280b3154
aDog <adrianmg3008@gmail.com>
2024-02-02 11:56:04 -0500

    add exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,32 +9,39 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleGoodClick = () => {
+    const newGood = good + 1;
+    setGood(newGood);
+    setTotal(newGood + neutral + bad);
+  };
+
+  const handleNeutralClick = () => {
+    const newNeutral = neutral + 1;
+    setNeutral(newNeutral);
+    setTotal(newNeutral + good + bad);
+  };
+
+  const handleBadClick = () => {
+    const newBad = bad + 1;
+    setBad(newBad);
+    setTotal(newBad + good + neutral);
+  };
 
   return (
     <div>
       <h2>give feedback</h2>
-      <Button
-        handleClick={() => {
-          setGood(good + 1);
-        }}
-        text="good"
-      />
-      <Button
-        handleClick={() => {
-          setNeutral(neutral + 1);
-        }}
-        text="neutral"
-      />
-      <Button
-        handleClick={() => {
-          setBad(bad + 1);
-        }}
-        text="bad"
-      />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good - bad) / total}</p>
+      <p>positive {(good * 100) / total}%</p>
     </div>
   );
 };
