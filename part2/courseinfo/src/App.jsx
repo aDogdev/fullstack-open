45371c9d28075504ec063945609d120a6563d8d6
aDog <adrianmg3008@gmail.com>
2024-02-03 11:57:25 -0500

    add exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,45 +1,4 @@
-function Header({ course }) {
-  return <h1>{course}</h1>;
-}
-
-function Total({ total }) {
-  return (
-    <p>
-      <strong>Number of exercises {total}</strong>
-    </p>
-  );
-}
-
-function Part({ part }) {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  );
-}
-
-function Content({ parts }) {
-  return (
-    <div>
-      {parts.map((part) => {
-        return <Part key={part.id} part={part} />;
-      })}
-    </div>
-  );
-}
-
-function Course({ course }) {
-  const total = course.parts.reduce((acc, el) => {
-    return acc + el.exercises;
-  }, 0);
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total total={total} />
-    </>
-  );
-}
+import { Course } from "./components/Course";
 
 function App() {
   const courses = [
