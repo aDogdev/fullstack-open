9dd01fc849a185effca6d1766e4c412fe1472074
aDog <adrianmg3008@gmail.com>
2024-02-03 15:42:58 -0500

    add exercise 2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,16 +2,22 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: 231321313 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+  const [showAll, setShowAll] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const newPerson = {
       name: newName,
       number: newNumber,
+      id: persons.length + 1,
     };
 
     if (persons.find((element) => element.name === newName)) {
@@ -20,12 +26,27 @@ const App = () => {
     }
     setPersons([...persons, newPerson]);
     setNewName("");
-    setNewNumber("")
+    setNewNumber("");
+  };
+
+  const personsToShow = showAll
+    ? persons
+    : persons.filter((person) =>
+        person.name.toLowerCase().includes(filter.toLowerCase())
+      );
+
+  const handleChange = (e) => {
+    const newFilter = e.target.value;
+    setFilter(newFilter);
+    newFilter === "" ? setShowAll(true) : setShowAll(false);
   };
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={handleChange} />
+      </div>
       <form onSubmit={handleSubmit}>
         <div>
           name:{" "}
@@ -44,13 +65,14 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => {
-          return (
-            <li key={person.name}>
-              {person.name} {person.number}
-            </li>
-          );
-        })}
+        {personsToShow &&
+          personsToShow.map((person) => {
+            return (
+              <li key={person.id}>
+                {person.name} {person.number}
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
